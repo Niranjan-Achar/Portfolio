b1b0582d2f148873a9a6e41306316ca02dcecc70
ninitro3 <ninitrogen@gmail.com>
2023-11-10 19:00:03 +0530

    6th -8

--- script.js
+++ script.js
@@ -58,3 +58,27 @@ const typed = new Typed('.multi-text',{
 })
 
 
+// View Counter using Local Storage itseems🤧🤧
+
+var counterContainer = document.querySelector(".views");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+    visitCount = Number(visitCount) + 1;
+    localStorage.setItem("page_view", visitCount);
+} else {
+    visitCount = 1;
+    localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
+
+// Adding onClick event listener
+resetButton.addEventListener("click", () => {
+    visitCount = 1;
+    localStorage.setItem("page_view", 1);
+    counterContainer.innerHTML = visitCount;
+});
+
+
